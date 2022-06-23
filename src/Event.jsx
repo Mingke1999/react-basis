88123d327b38ae0ef0197ce89e41bade55a0f7f9
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-23 12:47:24 +1000

    !this.state.flag

--- src/Event.jsx
+++ src/Event.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react'
 import "./event.css"
 export default class Event extends Component {
 
+  constructor(){
+    super();
+    this.state ={
+        flag:false
+    }
+  }
   clickHandle = () =>{
     console.log("hello")
   }
   moveHandle=()=>{
     console.log("mouse is moving inside box")
+  }
+  flagHandle=()=>{
+    this.setState({
+            flag:!this.state.flag
+        })
   }
   render() {
     return (
@@ -15,6 +26,12 @@ export default class Event extends Component {
         <button onClick={this.clickHandle}>clickHandle</button>
         {/**class->className */}
         <div className='box' onMouseMove={this.moveHandle}></div>
+        <div>
+            <button onClick={this.flagHandle.bind(this)}>change flag</button>
+            {
+                this.state.flag ? 'flagTrue':'flagFalse'
+            }
+        </div>
       </div>
     )
   }
