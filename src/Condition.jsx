ec608df183950b04ef4dde5aeeaff766fa9c7eae
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-23 11:28:02 +1000

    finish conditions

--- src/Condition.jsx
+++ src/Condition.jsx
@@ -3,9 +3,18 @@ import React, { Component } from 'react'
 export default class Condition extends Component {
   render() {
     const flag = true;
+    const msg ={
+       // title:"a short message"
+    }
     return (
       <div>
         {flag ? "isflaged":"notflaged"}
+        {
+            msg.title ? //whether exit
+            <p>{msg.title}</p>
+            : <div>loading content</div>
+        }
+        
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Hello from './Hello';
 import Time from './Time';
+import Condition from './Condition';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 function tick(){
   root.render(
     <div>
        <Hello/>
        <Time/>
+       <Condition/>
     </div>
  );
 }
