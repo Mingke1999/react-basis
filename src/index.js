c0b4fc91764f82e259b577bfaf9fbb5f2cfc5a85
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-06-23 13:23:32 +1000

    change folder level

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Time from './Time';
 import Event from './Event';
 var details ={
   title : "Details Page",
@@ -12,15 +11,11 @@ var users ={
   desc : "user list"
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function tick(){
-  root.render(
-    <div>
-      <Time/>
-      {/*<Props title={details.title} desc={details.desc} list={details.list}/>
-      <Props title={users.title} desc={users.desc}/>
-      <State/>*/}
-      <Event/>
-    </div>
- );
-}
-setInterval(tick,1000)
+root.render(
+  <div>
+    {/*<Props title={details.title} desc={details.desc} list={details.list}/>
+    <Props title={users.title} desc={users.desc}/>
+    <State/>*/}
+    <Event/>
+  </div>
+);
